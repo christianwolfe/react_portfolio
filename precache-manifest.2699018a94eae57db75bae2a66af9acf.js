9e043a874ff39d95446fd3c636fc0e7008f492a4
Christian Wolfe <cwolfe.k@gmail.com>
2020-08-13 23:53:06 -0400

    Updates

---
+++ precache-manifest.2699018a94eae57db75bae2a66af9acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996670eafea73b4ffe9821efab45395d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c3d3058e16c7e43fdd6b",
+    "url": "/react-portfolio/static/css/main.0985af52.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/react-portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d3058e16c7e43fdd6b",
+    "url": "/react-portfolio/static/js/main.d8c3ffaa.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
